d41f96980246362e26fc9a9f939c045f2f143782
Muhammad Kashif <muhammad.kashif72462@gmail.com>
2026-02-12 18:10:18 +0500

    Add core service providers for Vercel compatibility

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Illuminate\Filesystem\FilesystemServiceProvider::class,
+    Illuminate\Events\EventServiceProvider::class,
     Illuminate\View\ViewServiceProvider::class,
 ];
